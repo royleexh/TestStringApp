22ab751b10f0d590d1871cca6fcab2ac15e811cc
royleexh <271173141@qq.com>
2013-02-06 14:54:04 +0800

    hundred

--- src/com/test/SampleStringTest.java
+++ src/com/test/SampleStringTest.java
@@ -1,5 +1,7 @@
 package com.test;
 
+import java.util.Locale;
+
 import junit.framework.TestCase;
 
 /**
@@ -18,15 +20,15 @@ public class SampleStringTest extends TestCase{
 	}
 	
 	/*有效值min*/
-	public void testValidCharAt1(){
+	public void testCharAt1(){
 		String str = "abc12$%";
-		assertEquals("testValidCharAt1", 'a', str.charAt(0));
+		assertEquals("testCharAt1", 'a', str.charAt(0));
 	}
 	
 	/*有效值max*/
-	public void testValidCharAt2(){
+	public void testCharAt2(){
 		String str = "abc12$%";
-		assertEquals("testValidCharAt2", '%', str.charAt(str.length()-1));
+		assertEquals("testCharAt2", '%', str.charAt(str.length()-1));
 	}
 	
 	/*无效值lesser*/
@@ -42,15 +44,15 @@ public class SampleStringTest extends TestCase{
 	}
 	
 	/*有效值min*/
-	public void testValidCodePointAt1(){
+	public void testCodePointAt1(){
 		String str = "abc12$%";
-		assertEquals("testValidCodePointAt1", 97, str.codePointAt(0));
+		assertEquals("testCodePointAt1", 97, str.codePointAt(0));
 	}
 	
 	/*有效值max*/
-	public void testValidCodePointAt2(){
+	public void testCodePointAt2(){
 		String str = "abc12$%";
-		assertEquals("testValidCodePointAt2", 37, str.codePointAt(str.length()-1));
+		assertEquals("testCodePointAt2", 37, str.codePointAt(str.length()-1));
 	}
 	
 	/*无效值lesser*/
@@ -66,13 +68,13 @@ public class SampleStringTest extends TestCase{
 	}
 	
 	/*有效值min*/
-	public void testValidCodePointBefore1(){
+	public void testCodePointBefore1(){
 		String str = "abc12$%";
 		assertEquals("testInvalidCodePointAt2", 97, str.codePointBefore(1));
 	}
 	
 	/*有效值max*/
-	public void testValidCodePointBefore2(){
+	public void testCodePointBefore2(){
 		String str = "abc12$%";
 		assertEquals("testInvalidCodePointAt2", 37, str.codePointBefore(str.length()));
 	}
@@ -84,15 +86,15 @@ public class SampleStringTest extends TestCase{
 	}
 	
 	/*无效值more*/
-	public void testinValidCodePointBefore2(){
+	public void testInvalidCodePointBefore2(){
 		String str = "abc12$%";
-		assertEquals("testinValidCodePointBefore2", 0, str.codePointBefore(str.length()+1));
+		assertEquals("testInvalidCodePointBefore2", 0, str.codePointBefore(str.length()+1));
 	}
 	
 	/*有效值*/
-	public void testValidCodePointCount(){
+	public void testCodePointCount(){
 		String str = "abc12$%";
-		assertEquals("testValidCodePointCount", 7, str.codePointCount(0,str.length()));
+		assertEquals("testCodePointCount", 7, str.codePointCount(0,str.length()));
 	}
 	
 	/*无效值*/
@@ -102,10 +104,10 @@ public class SampleStringTest extends TestCase{
 	}
 	
 	/*有效值*/
-	public void testValidCompareTo(){
+	public void testCompareTo(){
 		String str1 = "hello";
-		String str2 = "hellp";
-		assertEquals("testValidCompareTo", 0, str1.compareTo(str2));
+		String str2 = "hello";
+		assertEquals("testCompareTo", 0, str1.compareTo(str2));
 	}
 	
 	/*无效值*/
@@ -115,10 +117,10 @@ public class SampleStringTest extends TestCase{
 	}
 	
 	/*有效值*/
-	public void testValidCompareToIgnoreCase(){
+	public void testCompareToIgnoreCase(){
 		String str1 = "hello";
 		String str2 = "HELlo";
-		assertEquals("testValidCompareToIgnoreCase", 0, str1.compareToIgnoreCase(str2));
+		assertEquals("testCompareToIgnoreCase", 0, str1.compareToIgnoreCase(str2));
 	}
 	
 	/*无效值*/
@@ -128,7 +130,7 @@ public class SampleStringTest extends TestCase{
 	}
 	
 	/*有效值*/
-	public void testValidConcat(){
+	public void testConcat(){
 		String str1 = "Hello";
 		String str2 = "java";
         assertEquals("testConcat", "Hellojava", str1.concat(str2));
@@ -141,10 +143,10 @@ public class SampleStringTest extends TestCase{
 	}
 	
 	/*有效值*/
-	public void testValidContains(){
+	public void testContains(){
 		String str1 = "hello";
 		String str2 = "el";
-		assertTrue("testValidContains", str1.contains(str2));
+		assertTrue("testContains", str1.contains(str2));
 	}
 	
 	/*无效值*/
@@ -153,11 +155,18 @@ public class SampleStringTest extends TestCase{
 		assertTrue("testInvalidContains", str.contains(null));
 	}
 	
-	/*有效值*/
-	public void testValidContentEquals(){
+	/*有效值1*/
+	public void testContentEquals1(){
 		String str1 = "hello";
-		String str2 = "el";
-		assertTrue("testValidContentEquals", str1.contentEquals(str2));
+		String str2 = "hello";
+		assertTrue("testContentEquals1", str1.contentEquals(str2));
+	}
+	
+	/*有效值2*/
+	public void testContentEquals2(){
+		String str1 = "hello";
+		StringBuffer str2 = new StringBuffer("hello");
+		assertTrue("testContentEquals2", str1.contentEquals(str2));
 	}
 	
 	/*无效值*/
@@ -166,60 +175,224 @@ public class SampleStringTest extends TestCase{
 		assertTrue("testInvalidContentEquals", str1.contentEquals(null));
 	}
 	
-	public void testEndsWith(){
-		String str = "hello";
-        String s = "lo";
-        assertTrue("testEndsWith", str.endsWith(s));
+	/*有效值1*/
+	public void testCopyValueOf1(){
+		char[] data = {'a','b','c','d'};
+		assertEquals("testCopyValueOf1", "abcd", String.copyValueOf(data));
+	}
+
+	/*有效值2*/
+	public void testCopyValueOf2(){
+		char[] data = {'a','b','c','d'};
+		assertEquals("testCopyValueOf2", "c", String.copyValueOf(data, 2, 1));
 	}
 	
+	/*无效值1*/
+	public void testInvalidCopyValueOf1(){
+		assertEquals("testInvalidCopyValueOf1", "abcd", String.copyValueOf(null));
+	}
+	
+	/*无效值2*/
+	public void testInvalidCopyValueOf2(){
+		char[] data = {'a','b','c','d'};
+		assertEquals("testInvalidCopyValueOf2", "abcd", String.copyValueOf(data, 2, 3));
+	}
+	
+	/*有效值1*/
+	public void testEndsWith1(){
+		String str = "hello";
+        String s = "lo";
+        assertTrue("testEndsWith1", str.endsWith(s));
+	}
+
+	/*有效值2*/
+	public void testEndsWith2(){
+		String str = "hello";
+		String s = "";
+		assertTrue("testEndsWith2", str.endsWith(s));
+	}
+	
+	/*无效值*/
+	public void testInvalidEndsWith(){
+		String str = "hello";
+		String s = null;
+		assertTrue("testInvalidEndsWith", str.endsWith(s));
+	}
+	
+	/*有效值*/
 	public void testEquals(){
-		String a = "String";
+		String a = "string";
         String b = "string";
         assertTrue("testEquals", a.equals(b));
 	}
 	
+	/*无效值*/
+	public void testInvalidEquals(){
+		String a = "String";
+		String b = null;
+		assertTrue("testInvalidEquals", a.equals(b));
+	}
+	
+	/*有效值*/
 	public void testEqualsIgnoreCase(){
 		String a = "String";
 		String b = "STRING";
 		assertTrue("testEqualsIgnoreCase", a.equalsIgnoreCase(b));
 	}
 	
+	/*无效值*/
+	public void testInvalidEqualsIgnoreCase(){
+		String a = "String";
+		String b = null;
+		assertTrue("testInvalidEqualsIgnoreCase", a.equalsIgnoreCase(b));
+	}
+	
+	/*有效值*/
+	public void testFormat(){
+		
+	}
+	
+	/*无效值*/
+	public void testInvalidFormat(){
+		
+	}
+	
+	/*有效值*/
+	public void testGetBytes(){
+		
+	}
+	
+	/*无效值*/
+	public void testInvalidGetBytes(){
+		
+	}
+	
+	/*有效值*/
+	public void testGetChars(){
+		String str = "Str";
+		char[] ch = {'a','b','c'};
+		str.getChars(0, str.length(), ch, 0);
+		System.out.println(ch);
+	}
+	
+	/*无效值,srcBegin 为负*/
+	public void testInvalidGetChars1(){
+		String str = "Str";
+		char[] ch = {'a','b','c'};
+		str.getChars(-1, 2, ch, 0);
+		System.out.println(ch);
+	}
+	
+	/*无效值,srcBegin 大于 srcEnd*/
+	public void testInvalidGetChars2(){
+		String str = "Str";
+		char[] ch = {'a','b','c'};
+		str.getChars(2, 1, ch, 0);
+		System.out.println(ch);
+	}
+	
+	/*无效值,srcEnd 大于此字符串的长度*/
+	public void testInvalidGetChars3(){
+		String str = "Str";
+		char[] ch = {'a','b','c'};
+		str.getChars(0, str.length()+1, ch, 0);
+		System.out.println(ch);
+	}
+	
+	/*无效值,dstBegin 为负*/
+	public void testInvalidGetChars4(){
+		String str = "Str";
+		char[] ch = {'a','b','c'};
+		str.getChars(0, str.length(), ch, -1);
+		System.out.println(ch);
+	}
+	
+	/*无效值,dstBegin+(srcEnd-srcBegin) 大于 dst.length*/
+	public void testInvalidGetChars5(){
+		String str = "Stri";
+		char[] ch = {'a','b','c'};
+		str.getChars(0, str.length(), ch, 0);
+		System.out.println(ch);
+	}
+	
 	public void testHashCode(){
 		String str = "String";
-		assertEquals("testHashCode", 123 , str.hashCode());
+		assertEquals("testHashCode", -1808118735 , str.hashCode());
 	}
 	
-	public void testIndexOf(){
+	/*有效值1*/
+	public void testIndexOf1(){
 		String str = "String";
-		assertEquals("testIndexOf",3 , str.indexOf("i"));
+		assertEquals("testIndexOf1", 3, str.indexOf("i"));
 	}
 	
-	public void testLastIndexOf(){
-		String str = "Stiring";
-		assertEquals("testLastIndexOf", 4 , str.lastIndexOf("i"));
+	/*有效值，不存在的索引为-1*/
+	public void testIndexOf2(){
+		String str = "S";
+		assertEquals("testIndexOf2", -1, str.indexOf("i"));
 	}
 	
+	/*有效值,""的索引为0*/
+	public void testIndexOf3(){
+		String str = "String";
+		assertEquals("testIndexOf2", 0, str.indexOf(""));
+	}
+	
+	/*无效值*/
+	public void testInvalidIndexOf(){
+		String str = "String";
+		assertEquals("testInvalidIndexOf", 3, str.indexOf(null));
+	}
+	
+	public void testIntern(){
+		String str = "String";
+		assertSame("testIntern", str, str.intern());
+		assertSame("testIntern", "String", str.intern());
+	}
+
 	public void testIsEmpty(){
-		String str = "String";
+		String str = "";
 		assertTrue("testIsEmpty", str.isEmpty());
+	}
+	
+	/*有效值*/
+	public void testLastIndexOf1(){
+		String str = "Stiring";
+		assertEquals("testLastIndexOf1", 4, str.lastIndexOf("i"));
+	}
+	
+	/*有效值，""的索引为str.length()*/
+	public void testLastIndexOf2(){
+		String str = "Stiring";
+		assertEquals("testLastIndexOf2", str.length(), str.lastIndexOf(""));
+	}
+	
+	/*无效值*/
+	public void testInvalidLastIndexOf(){
+		String str = "Stiring";
+		assertEquals("testInvalidLastIndexOf", 4, str.lastIndexOf(null));
 	}
 	
 	public void testlength(){
 		String str = "String";
 		assertEquals("testlength", 6, str.length());
 	}
+	
+	public void testmatches(){
+		
+	}
 
 	/*有效值1*/
-	public void testValidOffsetByCodePoints1(){
+	public void testOffsetByCodePoints1(){
 		String str = "abc12$%";
-		assertEquals("testValidOffsetByCodePoints1", 2, str.offsetByCodePoints(0,2));
+		assertEquals("testOffsetByCodePoints1", 2, str.offsetByCodePoints(0,2));
 	}
 	
 	/*有效值2*/
-	public void testValidOffsetByCodePoints2(){
+	public void testOffsetByCodePoints2(){
 		String str = "abc12$%";
 		System.out.println(str.length());
-		assertEquals("testValidOffsetByCodePoints2", 5, str.offsetByCodePoints(str.length(),-2));
+		assertEquals("testOffsetByCodePoints2", 5, str.offsetByCodePoints(str.length(),-2));
 	}
 	
 	/*无效值,index 为负*/
@@ -246,65 +419,270 @@ public class SampleStringTest extends TestCase{
 		assertEquals("testInvalidOffsetByCodePoints4", 0, str.offsetByCodePoints(0,-3));
 	}
 	
+	/*有效值*/
+	public void testRegionMatches(){
+		String str1 = "String";
+		String str2 = "javaString";
+		assertTrue("testRegionMatches", str1.regionMatches(0, str2, 4, str1.length()));
+	}
+	
+	/*无效值，thisStart为负*/
+	public void testInvalidRegionMatches1(){
+		String str1 = "String";
+		String str2 = "javaString";
+		assertTrue("testInvalidRegionMatches1", str1.regionMatches(-1, str2, 4, str1.length()));
+	}
+	
+	/*无效值，start为负*/
+	public void testInvalidRegionMatches2(){
+		String str1 = "String";
+		String str2 = "javaString";
+		assertTrue("testInvalidRegionMatches2", str1.regionMatches(0, str2, -1, str1.length()));
+	}
+	
+	/*无效值，thisStart+lengrh 大于此 str1 对象的长度*/
+	public void testInvalidRegionMatches3(){
+		String str1 = "String";
+		String str2 = "javaString";
+		assertTrue("testInvalidRegionMatches3", str1.regionMatches(2, str2, 4, str1.length()));
+	}
+	
+	/*无效值，start+lengrh 大于此 str2对象的长度*/
+	public void testInvalidRegionMatches4(){
+		String str1 = "String";
+		String str2 = "javaString";
+		assertTrue("testInvalidRegionMatches4", str1.regionMatches(0, str2, 5, str1.length()));
+	}
+	
 	public void testReplace(){
-		String str1="abfddddddd";
-        String str2="ccc";
-        String str=str1.replace("ddd",str2);
-        assertEquals("testReplace", "abfccccccd", str);
+		String str1 = "abfddddddd";
+        String str2 = "ccc";
+        assertEquals("testReplace", "abfccccccd", str1.replace("ddd",str2));
+	}
+	
+	public void testReplaceAll(){
+		String str = "a.b123456";   
+		assertEquals("testReplaceAll", "a.b12*456", str.replaceAll("3", "*"));
+		assertEquals("testReplaceAll", "a.b******", str.replaceAll("\\d", "*"));
 	}
 
 	public void testReplaceFirst(){
-		 String str="a.b?c,d%gd'fd";   
-         String str1=str.replaceFirst("\\?","jun");
-         String str2=str.replaceFirst("\\.","jun");
-         assertEquals("testReplaceFirst", "a.bjunc,d%gd'fd", str1);
-         assertEquals("testReplaceFirst", "ajunb?c,d%gd'fd", str2);
+		 String str = "a.b?c,?d%g.d'fd";   
+         String str1 = str.replaceFirst("\\?","jun");
+         String str2 = str.replaceFirst("\\.","jun");
+         assertEquals("testReplaceFirst", "a.bjunc,?d%g.d'fd", str1);
+         assertEquals("testReplaceFirst", "ajunb?c,?d%g.d'fd", str2);
 	}
 	
-	public void testSplit(){
-		String str="a:b:c:d:e:F";
-        String[]str1=str.split(":");
+	/*有效值1*/
+	public void testSplit1(){
+		String str = "a:b:c:d:e:F";
+        String[]str1 = str.split(":");
         for(int i=0;i<str1.length;i++){
-            System.out.print(str1[i]);
+            System.out.println(str1[i]);
         }
 	}
 	
-	public void teststartsWith(){
-		String str = "String";
-		assertTrue("teststartsWith", str.startsWith("abc"));
+	/*有效值2*/
+	public void testSplit2(){
+		String str = "boo:and:foo";
+		String[]str1 = str.split(":",2);
+		System.out.println(str1[0]);
+		System.out.println(str1[1]);
 	}
 	
-	public void testSubstring(){
+	/*有效值1*/
+	public void testStartsWith1(){
+		String str = "hello";
+        String s = "he";
+        assertTrue("testStartsWith1", str.startsWith(s));
+	}
+
+	/*有效值2*/
+	public void testStartsWith2(){
+		String str = "hello";
+		String s = "";
+		assertTrue("testStartsWith2", str.startsWith(s));
+	}
+	
+	/*无效值*/
+	public void testInvalidStartsWith2(){
+		String str = "hello";
+		String s = null;
+		assertTrue("testInvalidEndsWith", str.startsWith(s));
+	}
+	
+	/*有效值,一个参数*/
+	public void testSubstring1(){
 		String str="hellochinese";
-        String s=str.substring(0,3);
-        assertEquals("hell", s);
+        assertEquals(
+        		"testSubstring1", 
+        		"lochinese", 
+        		str.substring(3));
+	}
+	
+	/*有效值，两个参数*/
+	public void testSubstring2(){
+		String str="hellochinese";
+		assertEquals(
+				"testSubstring2", 
+				"hel", 
+				str.substring(0,3));
+	}
+
+	/*无效值,一个参数*/
+	public void testInvalidSubstring(){
+		String str="hell";
+		assertEquals(
+				"testInvalidSubstring", 
+				"lochinese", 
+				str.substring(9));
+	}
+	
+	/*无效值,beginIndex为负*/
+	public void testInvalidSubstring1(){
+		String str="hellochinese";
+		assertEquals(
+				"testInvalidSubstring1",
+				"hell",
+				str.substring(-1,3));
+	}
+	
+	/*无效值,endIndex 为负*/
+	public void testInvalidSubstring2(){
+		String str="hellochinese";
+		assertEquals(
+				"testInvalidSubstring2",
+				"hell",
+				str.substring(0,-1));
+	}
+	
+	/*无效值,endIndex 大于 length()*/
+	public void testInvalidSubstring3(){
+		String str="hellochinese";
+		assertEquals(
+				"testInvalidSubstring3",
+				"hell",
+				str.substring(0,str.length()+1));
+	}
+	
+	/*无效值, beginIndex 大于 endIndex*/
+	public void testInvalidSubstring4(){
+		String str="hellochinese";
+		assertEquals(
+				"testInvalidSubstring4",
+				"hell",
+				str.substring(2,1));
 	}
 	
 	public void testToCharArray(){
 		 String str = "Hello";
-         char[] A = new char[5];
-         A = str.toCharArray();
-         for(int i = 0 ; i < A.length ; i++){
-        	 System.out.println(A[i]);
+         char[] new_str = str.toCharArray();
+         for(int i = 0 ; i < new_str.length ; i++){
+        	 System.out.println(new_str[i]);
          }
 	}
 
 	public void testToLowerCase(){
-		String s="ABdFGjke123";
-        String A=s.toLowerCase();
-        assertEquals("testToLowerCase", "abdfgjke123", A);
+		String str="ABdFGjke123";
+        assertEquals("testToLowerCase", "abdfgjke123", str.toLowerCase(Locale.CHINESE));
 	}
 	
 	public void testToUpperCase(){
-		String s="ABdFGjke123";
-        String A=s.toUpperCase();
-        assertEquals("testToUpperCase", "ABDFGJKE123", A);
+		String str="ABdFGjke123";
+        assertEquals("testToUpperCase", "ABDFGJKE123", str.toUpperCase(Locale.CHINESE));
 	}
-	
+
 	public void testTrim(){
 		String str="            hi   hi   hi            ";   
-        String str1=str.trim();
-        assertEquals("testTrim", "hi   hi   hi", str1);
+        assertEquals("testTrim", "hi   hi   hi", str.trim());
+	}
+	
+	/*boolean*/
+	public void testValueOfBoolean(){
+		assertEquals(
+				"testValueOfBoolean", 
+				"true", 
+				String.valueOf(true));
+	}
+	
+	/*char*/
+	public void testValueOfChar(){
+		assertEquals(
+				"testValueOfChar", 
+				"a", 
+				String.valueOf('a'));
+	}
+	
+	/*char[]*/
+	public void testValueOfCharArray1(){
+		char[] charArray = {'a','b','c'};
+		assertEquals(
+				"testValueOfCharArray1", 
+				"abc", 
+				String.valueOf(charArray));
+	}
+	
+	/*valueOf(char[] data, int offset, int count)*/
+	public void testValueOfCharArray2(){
+		char[] charArray = {'a','b','c','d','e','f'};
+		assertEquals(
+				"testValueOfCharArray2", 
+				"ab", 
+				String.valueOf(charArray, 0, 2));
+	}
+	
+	/*double*/
+	public void testValueOfDouble(){
+		double num = 12.512;
+		assertEquals(
+				"testValueOfDouble", 
+				"12.512", 
+				String.valueOf(num));
+	}
+	
+	/*float*/
+	public void testValueOfFloat(){
+		float num = 12.512f;
+		assertEquals(
+				"testValueOfFloat", 
+				"12.512", 
+				String.valueOf(num));
+	}
+	
+	/*int*/
+	public void testValueOfInt(){
+		int num = 12;
+		assertEquals(
+				"testValueOfInt", 
+				"12", 
+				String.valueOf(num));
+	}
+	
+	/*long*/
+	public void testValueOfLong(){
+		long num = 12123;
+		assertEquals(
+				"testValueOfLong", 
+				"12123", 
+				String.valueOf(num));
+	}
+	
+	/*Object*/
+	public void testValueOfObject(){
+		Integer num = 12345;
+		assertEquals(
+				"testValueOfObject", 
+				"12345", 
+				String.valueOf(num));
+	}
+	
+	/*无效值*/
+	public void testInvalidValueOfBoolean(){
+		assertEquals(
+				"testInvalidValueOfBoolean", 
+				"true", 
+				String.valueOf(null));
 	}
 	
 	//========================combination==========================
@@ -314,7 +692,7 @@ public class SampleStringTest extends TestCase{
 		assertEquals("testCompareTo", 97, str.codePointAt(str.indexOf("a")));
 	}
 	
-	public void testCombination1(){
+	public void testCombineSubstring$indexOf(){
 		String str = "SampleStringTest.java";
 		String subStr = str.substring(0, str.indexOf("ing"));
 		assertEquals("", "SampleStr", subStr);
